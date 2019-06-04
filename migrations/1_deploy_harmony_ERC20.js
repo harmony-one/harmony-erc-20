39c259ae6acb668b03cc7fe14c72914d9c2cb3b1
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-06-03 17:02:13 -0700

    - owner can be any address now, instead of msg.sender - test deployment on rinkeby

--- migrations/1_deploy_harmony_ERC20.js
+++ migrations/1_deploy_harmony_ERC20.js
@@ -5,15 +5,27 @@ module.exports = function(deployer, network, accounts) {
   const symbols = "ONE"
   const decimals = 18
   const amount = 12600000000
-  const tokens = web3.utils.toWei(amount.toString(), 'ether')
   const ratio_minted = 0.0001
   const mint_tokens = amount * ratio_minted
   const mint_tokens_wei = web3.utils.toWei(mint_tokens.toString(), 'ether')
+  const owner = "0x44F2C713F788843c030f95824Dcc7c52Ad2e17c2"
 
   deployer.then(function() {
     return deployer.deploy(HarmonyERC20, name, symbols, decimals, mint_tokens_wei)
   }).then(function(instance) {
-    // instance.transfer("0x27C2D80C28115868f08D7960bc947c8022C7f965", mint_tokens_wei);
+    instance.transfer(owner, mint_tokens_wei);
+    return instance
+  }).then(function(instance) {
+    instance.addMinter(owner);
+    return instance
+  }).then(function(instance) {
+    instance.addPauser(owner);
+    return instance
+  }).then(function(instance) {
+    instance.renounceMinter();
+    return instance
+  }).then(function(instance) {
+    instance.renouncePauser();
   });
 
   //let instance = await HarmonyERC20.deployed()
--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,25 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const privateKey = "";
+const tokenKey = "";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      host: "localhost",
+      provider: function() {
+        return new HDWalletProvider(privateKey, "https://rinkeby.infura.io/v3/" + tokenKey)
+      },
+      network_id: 4,
+      // gas: 67500000,
     }
   },
     // Configure your compilers
