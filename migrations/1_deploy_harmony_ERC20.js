a0a86ffd689bdb0f105cc5b439cc877dfc74f01c
Nicolas Burtey <nicolas.burtey@gmail.com>
2019-06-10 15:15:39 -0700

    integrate bounty token allocation

--- migrations/1_deploy_harmony_ERC20.js
+++ migrations/1_deploy_harmony_ERC20.js
@@ -9,11 +9,17 @@ module.exports = function(deployer, network, accounts) {
   const mint_tokens = amount * ratio_minted
   const mint_tokens_wei = web3.utils.toWei(mint_tokens.toString(), 'ether')
   const owner = "0x44F2C713F788843c030f95824Dcc7c52Ad2e17c2"
+  const bounty_1_address = "0x00de4b13153673bcae2616b67bf822500d325fc3"
+  const bounty_1_amount = 18250.1
+  const bounty_1_amount_wei = web3.utils.toWei(bounty_1_amount.toString(), 'ether')
 
   deployer.then(function() {
     return deployer.deploy(HarmonyERC20, name, symbols, decimals, mint_tokens_wei)
   }).then(function(instance) {
-    instance.transfer(owner, mint_tokens_wei);
+    instance.transfer(bounty_1_address, bounty_1_amount_wei);
+    return instance
+  }).then(function(instance) {
+    instance.transfer(owner, mint_tokens_wei - bounty_1_amount_wei);
     return instance
   }).then(function(instance) {
     instance.addMinter(owner);
